6454d2a7e83e36595c51b8b63732fa863a368c54
Salazar <hector.salazar@encora.com>
2021-10-14 09:41:49 -0500

    Added validations and 2 callbacks
    In order to understand these two concepts

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,27 @@
 class Patient < ApplicationRecord
-  belongs_to :doctor
-  has_many :consultations #customary belongs_to before has_many
+  belongs_to :doctor # <- MANDATORY FIELD
+  belongs_to(:doctor)
+  has_many(:consultations) #customary belongs_to before has_many
 
-  def complain
+  validates :username,
+    presence: true,
+    length: { minimum: 4 }
+
+  before_save :announce_save
+  after_save :log_save
+
+  def complain(doctor_name)
     "Hey, doctor #{doctor.name}, my head hurts"
   end
+
+  private
+
+  def announce_save
+    puts "Hey, I'm saving patient #{username} here!"
+  end
+
+  def log_save
+    puts "I just saved patient #{id}"
+  end
+
 end
