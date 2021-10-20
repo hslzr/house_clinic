5852fdb7961c1e83aef051e8c3e05de6355901ba
David Ufberg <dufberg@sudufberg-m1.local>
2021-10-20 13:32:08 -0400

    adds validations to consultation model

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,4 +1,17 @@
 class Consultation < ApplicationRecord
     belongs_to :doctor # indicates that method must link to db consult
     belongs_to :patient 
+
+validates: :start_time, presence: true
+validates: :end_time, presence: true
+
+validates :valid_start_end_times?
+validates :start_time_after_now?
+
+def valid_start_end_times?
+    start.time.before? end_time
+end
+
+def start_time_after_now?
+    start_time.after? DateTime.now
 end
