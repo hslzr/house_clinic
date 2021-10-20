1d282d38e85a685949a71b4dc92abff78fad04ac
David Ufberg <dufberg@sudufberg-m1.local>
2021-10-20 13:37:17 -0400

    adds some validations to doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,6 +2,9 @@ class Doctor < ApplicationRecord
   has_many :patients  # optimizes query 
   has_many :consultations
 
+  validates: :name, presence: true, format: {with: [[:alnum:]]}
+  validates: :license_number, uniqueness: true
+
   def presentation
     "##{license_number}: #{name}"
   end
