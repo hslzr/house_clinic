c5b7ea7e0e96a51bab99e33d0d9b3c28199f0130
David Ufberg <dufberg@sudufberg-m1.local>
2021-11-09 16:18:49 -0500

    refactors patient and doctor specs with shoulda matchers

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,24 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  describe 'doctor must have a name' do 
-    it 'passes if name is present' do
-      doctor = Doctor.new
-
-      expect(doctor).not_to be_valid
-      expect(doctor.errors.full_messages).to include("Name can't be blank")
-    end
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name)}
+    it { is_expected.to validate_presence_of(:license_number)}
+    it { is_expected.to validate_uniqueness_of(:license_number)}
   end
+  #   describe 'doctor must have a name' do 
+#     it 'passes if name is present' do
+#       doctor = Doctor.new
+
+#       expect(doctor).not_to be_valid
+#       expect(doctor.errors.full_messages).to include("Name can't be blank")
+#     end
+#   end
   
-  describe 'doctor must have a unique license number' do
-    it 'passes if license number is unique' do
-      doctor1 = Doctor.create(name: "Bill", license_number: 1)
-      doctor2 = Doctor.create(name: "Jim", license_number: 1)
+#   describe 'doctor must have a unique license number' do
+#     it 'passes if license number is unique' do
+#       doctor1 = Doctor.create(name: "Bill", license_number: 1)
+#       doctor2 = Doctor.create(name: "Jim", license_number: 1)
 
-      expect(doctor2).not_to be_valid
-      expect(doctor2.errors.full_messages).to include("License number has already been taken")
-  end
-end
+#       expect(doctor2).not_to be_valid
+#       expect(doctor2.errors.full_messages).to include("License number has already been taken")
+#   end
+# end
 
   describe '#presentation' do
     it 'passes if presentation returns license number and name' do
@@ -28,5 +33,5 @@ end
     end 
   end
 
-  pending "add some examples to (or delete) #{__FILE__}"
+#   pending "add some examples to (or delete) #{__FILE__}"
 end
